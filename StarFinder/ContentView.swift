540dbacfea2b0a6758e71e22b7e64f417701d086
Jake <2bjake@users.noreply.github.com>
2022-01-18 12:37:25 -0600

    Initial Commit

---
+++ StarFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarFinder
+//
+//  Created by Jake Foster on 1/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarFinder/StarFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarFinderApp.swift
+//  StarFinder
+//
+//  Created by Jake Foster on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct StarFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
