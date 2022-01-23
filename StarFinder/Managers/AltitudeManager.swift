85d3fe818cb7a8f4b41fa69dfe8c17636ba897fd
Jake <2bjake@users.noreply.github.com>
2022-01-23 09:09:58 -0600

    fix altitude wrap around issue

--- StarFinder/Managers/AltitudeManager.swift
+++ StarFinder/Managers/AltitudeManager.swift
@@ -22,7 +22,7 @@ class AltitudeManager {
   private func handler(motion: CMDeviceMotion?, error: Error?) {
     guard error == nil, let motion = motion else { return }
     let rollRadians = motion.attitude.roll
-    let altitude = Angle(radians: -rollRadians - (.pi / 2)) // TODO: what to do for >90?
+    let altitude = Angle(radians: abs(rollRadians) - (.pi / 2))
     onAltitudeChange?(altitude)
   }
 
